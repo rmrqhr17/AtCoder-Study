878d502829d79f4fa4958f6a810387a17e2d23a5
rmrqhr17 <rmrqhr17@gmail.com>
2025-11-30 15:03:31 +0900

    A:AC

---
+++ ABC434/rmrqhr17/A.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+    using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int n,b;
+    cin >> n >> b;
+    n*=1000;
+    if (n%b==0) {
+        cout << n/b+1;
+    }
+    else {
+        cout << n/b+1;
+    }
+
+}
